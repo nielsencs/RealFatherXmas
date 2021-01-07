485cb92b1d328dfe203538ead918a2512a7208dd
Carl Nielsen <nielsencs@users.noreply.github.com>
2021-01-07 11:45:23 +0000

    Create zoom_index.php
    Potential start of zoom call selling page

---
+++ site/zoom_index.php
@@ -0,0 +1,30 @@
+<?php
+  require_once 'header.php';
+//  require_once 'timeStamp.php';
+?>
+        <div class="VisitSanta">
+            <p>Visit Santa from the comfort of your own home!</p>
+            <p>Book A Live Video Call</p>
+        </div>
+        <div class="ContactForm">
+            <h2>Contact Us</h2>
+            <form id="comp-khay9yrt" class="Cdk9j">
+                <label for="input_name" class="_2UrhM"></label>
+                <input type="text" name="name" id="input_name" value="" placeholder="Name" maxLength="100"/>
+
+                <label for="input_email" class="_2UrhM"></label>
+                <input type="email" name="email" id="input_email" value="" placeholder="Email" required="" pattern="^.+@.+\.[a-zA-Z]{2,63}$" maxLength="250"/>
+
+                <label for="input_comp-khay9ysz" class="_2UrhM"></label>
+                <input type="text" name="subject" id="input_comp-khay9ysz" class="A1_tY has-custom-focus" value="" placeholder="Subject"/>
+
+                <label for="textarea_comp-khay9yt2" class="_2SAi8"></label>
+                <textarea id="textarea_comp-khay9yt2" class="_1qIig has-custom-focus" placeholder="Type your message here..."></textarea>
+
+                <button data-testid="buttonElement" class="_1_kc0">Submit</button>
+                <p>Thanks for submitting!</p>
+            </form>
+        </div>
+<?php
+  require_once 'footer.php';
+?>
