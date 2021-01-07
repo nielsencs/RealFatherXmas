7b74175883c47626e1c89f8e391434452b598cb1
Carl Nielsen <nielsencs@users.noreply.github.com>
2021-01-07 10:51:36 +0000

    remove video - too bandwidth hungry!

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 ?>
         <div class="VisitSanta section">
           <h4>Santa's very tired.<br />Try popping back in November!</h4>
-          <div class="video-container">
+          <!--div class="video-container">
             <video autoplay loop muted>
               <source src="/media/SantaSnooze3.mp4" type=video/mp4>
             </video>
-          </div>
+          </div-->
         </div>
 <?php
   require_once 'footer.php';
