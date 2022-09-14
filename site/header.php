9c437824632e6fc107f6a1b9761637a81a3a2b43
nielsencs <nielsencs@users.noreply.github.com>
2022-09-14 11:31:27 +0100

    new email

--- site/header.php
+++ site/header.php
@@ -47,7 +47,7 @@
             </div>
             <div class="navBoxRight">
                 <div class="navLinks">
-                    <a href="mailto:realfatherxmas@ncsnet.co.uk">realfatherxmas@ncsnet.co.uk</a>
+                    <a href="mailto:elves@realfatherxmas.com">elves@realfatherxmas.com</a>
                     <a rel="noreferrer" href="https://www.facebook.com/realfatherxmas" target="_blank"><img src="images\Facebook.webp" alt="Facebook" class="FB"></a>
                 </div>
             </div>
