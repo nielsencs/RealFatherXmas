e7a4eb977014128decbe4ef9b8c271bc1cc2c283
nielsencs <nielsencs@users.noreply.github.com>
2022-09-26 22:22:11 +0100

    fun menu added

--- site/fun.php
+++ site/fun.php
@@ -3,19 +3,13 @@ $bSnow = true;
 require_once 'header.php';
 ?>
 <div class="main">
-    <h2>Welcome to our Fun Stuff</h2>
+<?php
+require_once 'funMenu.php';
+?>
     <p>If your budget is a bit too tight, here's a message from Mr C himself for you to show your loved ones with our very best!</p>
-    <iframe width="560"
-            height="315"
-            src="https://www.youtube.com/embed/CKWPQIMlYxo"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen></iframe>
-    <ul>
-        <li>Why Xmas?</li>
-        <li>All About Mr C&rsquo;s Reindeer</li>
-    </ul>
+    <div class="responsive-iframe-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/CKWPQIMlYxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 </div>
 <?php
 require_once 'footer.php';
