8b35920dac4d60f45268e067d7c76ece43910578
nielsencs <nielsencs@users.noreply.github.com>
2022-09-26 22:21:32 +0100

    embed Google calendar for events.

--- site/events.php
+++ site/events.php
@@ -4,58 +4,10 @@ require_once 'header.php';
 ?>
 <div class="main">
     <h2>Upcoming events</h2>
-    <table style="table-layout: fixed;">
-        <tr>
-            <th>When?</th>
-            <th>Where?</th>
-            <th>What?</th>
-        </tr>
-        <tr>
-            <td>Thu, Dec 1, 2022 10am - 5:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Fri, Dec 2, 2022 10am - 5:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Sat, Dec 3, 2022 10am - 12:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Wed, Dec 7, 2022 10am - 5:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Thu, Dec 8, 2022 10am - 5:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Fri, Dec 9, 2022 10am - 5:30pm</td>
-            <td>Way Inn, 268 High St, Berkhamsted HP4 1AQ</td>
-            <td>Regular appearance - See him in person for £10 per child.</td>
-        </tr>
-        <tr>
-            <td>Sat, Dec 10, 2022 10am - 5:30pm</td>
-            <td>TBA</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Sun, Dec 11, 2022 4pm - 5:30pm</td>
-            <td>Sandfields, Swansea</td>
-            <td>See him in the streets for FREE</td>
-        </tr>
-        <tr>
-            <td>Sun, Dec 11, 2022 4pm - 5:30pm</td>
-            <td>Legacy Church, Spring Terrace, Sandfields, Swansea SA1 3TD</td>
-            <td>See him after the service for FREE</td>
-        </tr>
-    </table>
+    <div class="responsive-iframe-container">
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&mode=AGENDA&hl=en_GB&showTitle=1&showNav=1&showDate=1&showPrint=1&showTabs=1&showCalendars=0&showTz=0&src=cmZ4ZWx2ZXNAZ21haWwuY29t&color=%238c9d52" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <!-- <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon                                                                           &showCalendars=0&showTz=0&src=cmZ4ZWx2ZXNAZ21haWwuY29t&color=%232f4d36" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>             -->
+    </div>
 </div>
 <?php
 require_once 'footer.php';
