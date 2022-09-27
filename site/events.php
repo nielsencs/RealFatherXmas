1280b21012e6577716079fdd1b18aec9eceb7224
nielsencs <nielsencs@users.noreply.github.com>
2022-09-27 18:55:16 +0100

    tidy up

--- site/events.php
+++ site/events.php
@@ -5,8 +5,7 @@ require_once 'header.php';
 <div class="main">
     <h2>Upcoming events</h2>
     <div class="responsive-iframe-container">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&mode=AGENDA&hl=en_GB&showTitle=1&showNav=1&showDate=1&showPrint=1&showTabs=1&showCalendars=0&showTz=0&src=cmZ4ZWx2ZXNAZ21haWwuY29t&color=%238c9d52" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-        <!-- <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon                                                                           &showCalendars=0&showTz=0&src=cmZ4ZWx2ZXNAZ21haWwuY29t&color=%232f4d36" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>             -->
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&mode=AGENDA&hl=en_GB&showTitle=1&showNav=1&showDate=1&showPrint=1&showTabs=1&showCalendars=0&showTz=0&src=cmZ4ZWx2ZXNAZ21haWwuY29t&color=%232f4d36" style="border-width:0" width="800" height="600"></iframe>
     </div>
 </div>
 <?php
