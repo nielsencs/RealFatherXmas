72110a390c05a560dad8ea77d0a500e67ff8d223
Carl Nielsen <nielsencs@users.noreply.github.com>
2023-01-05 19:17:52 +0000

    There he is!

--- site/header.php
+++ site/header.php
@@ -30,7 +30,7 @@
 <body>
     <header>
         <div class="logoBox">
-            <img class="logo" src="images/RealFatherXmasLogo3_TRANS.png" alt="RealFatherXmas Logo">
+        <img class="logo" src="images/IMG_6545_Face.png" alt="RealFatherXmas Logo">
 
             <h1>Real Father Xmas</h1>
             <p class="centerText" style="font-size: smaller;">I&nbsp;look&nbsp;like&nbsp;him. I&nbsp;sound&nbsp;like&nbsp;him. I&nbsp;am&nbsp;him!</p>
