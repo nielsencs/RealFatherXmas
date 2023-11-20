598069d0e27a6f2c0b81650a344341e98601373e
Carl Nielsen <nielsencs@users.noreply.github.com>
2023-11-20 13:16:07 +0000

    WiP

---
+++ site/zoomCalls.php
@@ -0,0 +1,69 @@
+<?php
+$bSnow = false;
+require_once 'header.php';
+?>
+<div class="main">
+    <h2>Zoom Calls</h2>
+    <div class="product">
+        <h3>5 Minute Zoom Call</h3>
+        <p>A five minute video chat for the whole family with the Real Father Xmas himself. Visit Mr C&apos;s grotto from the comfort of your own home!</p>
+        <p>If you have more than 3 children you might want to choose the 10 minute option.</p>
+        <p>Please ensure you have zoom already installed and let us know in advance if you have any specific requests.</p>
+        <p>
+        <a href="https://buy.stripe.com/test_28o9DG6A40WTeRy001">BUY NOW - only &pound;14.99!</a>
+        </p>
+    </div>
+    <div class="product">
+        <h3>10 Minute Zoom Call</h3>
+        <p>A five minute video chat for the whole family with the Real Father Xmas himself. Visit Mr C&apos;s grotto from the comfort of your own home!</p>
+        <p>If you have more than 3 children you might want to choose the 10 minute option.</p>
+        <p>Please ensure you have zoom already installed and let us know in advance if you have any specific requests.</p>
+        <p>
+        <a href="https://buy.stripe.com/test_28o9DG6A40WTeRy001">BUY NOW - only &pound;14.99!</a>
+        </p>
+    </div>
+    <div class="product">
+        <h3>15 Minute Zoom Call</h3>
+        <p>A five minute video chat for the whole family with the Real Father Xmas himself. Visit Mr C&apos;s grotto from the comfort of your own home!</p>
+        <p>If you have more than 3 children you might want to choose the 10 minute option.</p>
+        <p>Please ensure you have zoom already installed and let us know in advance if you have any specific requests.</p>
+        <p>
+        <a class="buttonClass" href="https://buy.stripe.com/test_28o9DG6A40WTeRy001">BUY NOW - only &pound;14.99!</a>
+        </p>
+    </div>
+    <div style="clear: all;"></div>
+</div>
+<?php
+require_once 'footer.php';
+?>
+
+
+
+5 Minute Zoom Call
+Have a nice chat with Mr C over Zoom for five Minutes
+A five minute zoom chat for the whole family with the Real Father Xmas. Visit Mr C’s grotto from the comfort of your own home!
+
+If you have more than 3 children you might want to choose the 10 minute option.
+
+Please ensure you have zoom already installed and let us know in advance if you have any specific requests.
+14.99
+
+
+10 Minute Zoom Call
+Have a nice 10 Minute chat with Mr C over Zoom
+A 10 minute zoom chat for the whole family with the Real Father Xmas! Visit Mr C’s grotto from the comfort of your own home!
+
+This option is ideal to have a more leisurely chat or if you have for around 4 to 7 children, for example if there are 2 families who want to be on the call at the same time.
+
+Please ensure you have zoom already installed and let us know in advance if you have any specific requests.
+24.99
+
+
+15 Minute Zoom Call
+Have a nice, leisurely Zoom chat with Mr C for fifteen Minutes
+A fifteen minute zoom chat for the whole family with the Real Father Xmas! Visit Mr C’s grotto from the comfort of your own home!
+
+This is the luxury option! If you have more than 3 groups wanting to join the call or you just want to luxuriate in Mr C's company, this is the one for you. You'll be surprised how much fun it is and how quickly a quarter of an hour with the great man himself will be over!
+
+Please ensure you have zoom already installed and let us know in advance if you have any specific requests.
+34.99
