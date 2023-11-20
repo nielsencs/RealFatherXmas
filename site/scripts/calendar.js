57ca0384f3a89ef06d4051a18f405cae20c6c72a
Carl Nielsen <nielsencs@users.noreply.github.com>
2023-11-20 13:11:37 +0000

    WiP

--- site/scripts/calendar.js
+++ site/scripts/calendar.js
@@ -1,11 +1,12 @@
 // Load the API client and authenticate with your API key or OAuth 2.0 client ID
 gapi.load('client', function() {
-    API_KEY = 'AIzaSyDbOhFJIKFUzqN2NXJAatJs2oYUFwpjNQE'
+    const API_KEY = 'AIzaSyDbOhFJIKFUzqN2NXJAatJs2oYUFwpjNQE'
+    const CLIENT_ID = '123758879436-3pof414llg0877db7mujrbeojugcaeuc.apps.googleusercontent.com'
 
     gapi.client.init({
       apiKey: API_KEY,
       discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-      clientId: 'YOUR_CLIENT_ID',
+      clientId: CLIENT_ID,
       scope: 'https://www.googleapis.com/auth/calendar.readonly'
     }).then(function() {
       // Call the Google Calendar API to retrieve the events
@@ -19,8 +20,17 @@ gapi.load('client', function() {
       });
     }).then(function(response) {
       var events = response.result.items;
-      // Format the events into HTML and display them on your website
-      // ...
+      // Format the events into an HTML table and display it on your website
+      var table = '<table>';
+      table += '<thead><tr><th>Event</th><th>Date</th></tr></thead>';
+      table += '<tbody>';
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var startDate = event.start.dateTime || event.start.date;
+        var endDate = event.end.dateTime || event.end.date;
+        table += '<tr><td>' + event.summary + '</td><td>' + startDate + ' - ' + endDate + '</td></tr>';
+      }
+      table += '</tbody></table>';
+      document.getElementById('calendar').innerHTML = table;
     });
   });
-  
