43bc2eacfa294aef36641febe93f31322fb7efa8
Carl Nielsen <nielsencs@users.noreply.github.com>
2025-07-21 16:08:03 +0100

    Update 404.php

--- site/404.php
+++ site/404.php
@@ -2,9 +2,10 @@
 require_once 'header.php';
 ?>
 <div class="main">
-    <h2>404 - Page Not Found</h2>
-    <p>Sorry, the page you are looking for does not exist.</p>
-    <p>You can use the navigation above to find what you're looking for.</p>
+    <h2>I&apos;m so sorry that page wasn&apos;t found</h2>
+    <p>The techy elves call it a &lsquo;404&rsquo; error.</p>
+    <p>I&apos;m sure the elves are really sorry that the page you were looking for does not exist.</p>
+    <p>All the currently available pages should be in the navigation above. I do hope you find what you're looking for.</p>
 </div>
 <?php
 require_once 'footer.php';
